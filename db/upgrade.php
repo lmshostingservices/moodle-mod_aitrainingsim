949cb78c8137ce3e3a3ceef1798383c1226794c9
LMS Labs Deploy <deploy@lms-labs.com>
2026-08-13 01:49:55 +0000

    release: mod_aitrainingsim v1.1.4

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,38 +1,9 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * Upgrade steps for mod_aitrainingsim.
- *
- * @package    mod_aitrainingsim
- * @copyright  2026 LMS-Labs
- * @license    http://www.gnu.org/licenses/gpl-3.0.html GNU GPL v3 or later
- */
-
 defined('MOODLE_INTERNAL') || die();
 
-/**
- * Execute mod_aitrainingsim upgrade from the given old version.
- *
- * @param int $oldversion
- * @return bool
- */
-function xmldb_mod_aitrainingsim_upgrade($oldversion) {
+function xmldb_aitrainingsim_upgrade($oldversion) {
     if ($oldversion < 2026073100) {
-        upgrade_plugin_savepoint(true, 2026073100, 'mod', 'aitrainingsim');
+        upgrade_mod_savepoint(true, 2026073100, 'aitrainingsim');
     }
     return true;
 }
